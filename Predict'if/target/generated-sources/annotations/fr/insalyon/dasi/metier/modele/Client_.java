84abb308703e2b3fffb82176fab86490df2a135b
aakaydin <37652644+aakaydin@users.noreply.github.com>
2020-04-05 15:12:43 +0300

    Add files via upload

---
+++ Predict'if/target/generated-sources/annotations/fr/insalyon/dasi/metier/modele/Client_.java
@@ -0,0 +1,22 @@
+package fr.insalyon.dasi.metier.modele;
+
+import java.util.Date;
+import javax.annotation.Generated;
+import javax.persistence.metamodel.SingularAttribute;
+import javax.persistence.metamodel.StaticMetamodel;
+
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2020-03-30T17:49:19")
+@StaticMetamodel(Client.class)
+public class Client_ { 
+
+    public static volatile SingularAttribute<Client, Date> dateDeNaissance;
+    public static volatile SingularAttribute<Client, Boolean> connected;
+    public static volatile SingularAttribute<Client, String> motDePasse;
+    public static volatile SingularAttribute<Client, String> numeroTel;
+    public static volatile SingularAttribute<Client, String> mail;
+    public static volatile SingularAttribute<Client, String> adresse;
+    public static volatile SingularAttribute<Client, Long> id;
+    public static volatile SingularAttribute<Client, String> nom;
+    public static volatile SingularAttribute<Client, String> prenom;
+
+}
